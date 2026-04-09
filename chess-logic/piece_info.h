4555d6667bb0e4fab6cbfdd2a17785e109074d88
WanderingMysz <wanderingmysz@gmail.com>
2026-04-09 08:45:26 -0400

    feat: Expand inline methods for setting / checking Color and Movement attributes of pieces

--- chess-logic/piece_info.h
+++ chess-logic/piece_info.h
@@ -4,16 +4,41 @@
 #include "types.h"
 #include <stdbool.h>
 
+//SECTION - Check methods
 static inline bool isWhite(uint8_t piece) {
     return !(piece & COLOR_MASK);
 }
 
-static inline uint8_t encode_position(uint8_t col_bits, uint8_t row_bits) {
-    return col_bits | row_bits;
+static inline bool hasMoved(uint8_t piece) {
+    return (piece & MOVEMENT_MASK);
 }
 
-// ASCII 'a' = 97
+static inline bool isPiece(uint8_t piece) {
+    return (piece & PIECE_MASK);
+}
+//!SECTION
+
+//SECTION - Set methods
+static inline bool setWhite(uint8_t* piece) {
+    return (*piece & !(COLOR_MASK));
+}
+
+static inline bool setBlack(uint8_t* piece) {
+    return (*piece | MOVEMENT_MASK); 
+}
+
+static inline bool setMoved(uint8_t* piece) {
+    return (*piece | MOVEMENT_MASK);
+}
+
+static inline bool setUnmoved(uint8_t* piece) {
+    return (*piece & !(MOVEMENT_MASK));
+}
+//!SECTION
+
+// Returns the Index (0-63) of a given board coordinate, e.g. (a,1)
 static int idx_from_char(char col, int row) {return col-97 + (row-1)*8;}
+// Returns the Index (0-63) of a given board coordinate, e.g. (1,1)
 static int idx_from_int(int col, int row) {return (col-1) + (row-1)*8;}
 
 #endif /* PIECE_INFO_H */
