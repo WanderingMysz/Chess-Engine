2279a9e7a54443535c71c0bf51756801644244c3
WanderingMysz <wanderingmysz@gmail.com>
2026-04-09 09:27:19 -0400

    refactor: Remove square as a separate struct. Chessboard is now simply an array of 64 uint8_t pieces

--- chess-logic/manipulate_board.c
+++ chess-logic/manipulate_board.c
@@ -1,25 +1,20 @@
 #include "manipulate_board.h"
 #include <stdio.h>
 
-void set_square(Square *sq, uint8_t position, uint8_t piece) {
-    sq->position = position;
-    sq->piece = piece;
+void set_square(Chessboard* board, int idx, uint8_t piece) {
+    board->squares[idx] = piece;
 }
+
 /* Sets the color of a given square's piece */
-static void set_piece_color(Square *sq, uint8_t color) {
-    sq->piece &= ~(COLOR_MASK);
-    sq->piece |= color;
+static void set_piece_color(uint8_t* piece, uint8_t color) {
+    *piece &= ~(COLOR_MASK);
+    *piece |= color;
 }
 
 static void _set_pawns(Chessboard *board, int row) {
     for (int col = 1; col <= 8; col++) {
-        uint8_t pos_col = COL_BITS(col);
-        uint8_t pos_row = ROW_BITS(row);
-        uint8_t pos = encode_position(pos_col, pos_row);
-
         uint8_t piece_type = PAWN;
-
-        set_square(&board->squares[idx_from_int(col, row)], pos, piece_type);
+        set_square(board, idx_from_int(col, row), piece_type);
     }
 }
 
@@ -30,10 +25,6 @@ static void set_pawns(Chessboard *board) {
 
 static void _set_pieces(Chessboard *board, int row) {
     for (int col = 1; col <= 8; col++) {
-        uint8_t pos_col = COL_BITS(col);
-        uint8_t pos_row = ROW_BITS(row);
-        uint8_t pos = encode_position(pos_col, pos_row);
-        
         uint8_t piece_type;
         switch (col) {
             case 1:
@@ -58,7 +49,7 @@ static void _set_pieces(Chessboard *board, int row) {
                 piece_type = NONE;
                 break;
         }
-        set_square(&board->squares[idx_from_int(col, row)], pos, piece_type);
+        set_square(board, idx_from_int(col, row), piece_type);
     }
 }
 
@@ -67,17 +58,13 @@ static void set_pieces(Chessboard *board) {
     _set_pieces(board, 8);
 }
 
-void set_colors(Chessboard *board) {
+static void set_colors(Chessboard *board) {
     for (int row = 1; row <= 8; row++) {
         uint8_t color = (row <= 4) ? WHITE : BLACK;
 
         for (int col = 1; col <= 8; col++) {
             int idx = idx_from_int(col, row);
-            uint8_t pos = encode_position(COL_BITS(col), ROW_BITS(row));
-
-            Square* curr_square = &board->squares[idx];
-            curr_square->position |= pos;
-            set_piece_color(curr_square, color);
+            set_piece_color(&(board->squares[idx]), color);
         }
     }
 }
@@ -93,8 +80,13 @@ Chessboard initialize_chessboard() {
     return board;
 }
 
+Chessboard initialize_empty_chessboard() {
+    Chessboard board = {0};
+    return board;
+}
+
 void clear_board(Chessboard *board) {
     for (int i = 0; i < BOARD_SIZE; i++) {
-        set_square(&board->squares[i], 0, NONE);
+        set_square(board, i, NONE);
     }
 }
