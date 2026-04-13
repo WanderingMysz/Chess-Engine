c3cfff814e978c0d27ef55e4a06e2ad8359491c5
WanderingMysz <wanderingmysz@gmail.com>
2026-04-13 00:32:30 -0400

    feat: Add move record format information

--- game-statistics/stats.cbl
+++ game-statistics/stats.cbl
@@ -1,19 +1,44 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       PROGRAM-ID. STATS.
+       AUTHOR. WanderingMysz.
 
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-           SELECT INPUT-FILE  ASSIGN TO "input.dat"
+           SELECT INPUT-FILE  ASSIGN TO "games.dat"
                ORGANIZATION IS LINE SEQUENTIAL.
            SELECT OUTPUT-FILE ASSIGN TO "output.dat"
                ORGANIZATION IS LINE SEQUENTIAL.
 
        DATA DIVISION.
        FILE SECTION.
-       FD INPUT-FILE.
-       01 INPUT-RECORD.
-          05 FILLER PIC X(80).
+       FD GAME-FILE.
+       01 MOVE-RECORD.
+           05 TURN_NUMBER PIC 9999.
+           05 PLAYER_COLOR PIC X.
+               88 WHITE VALUE 'W'.
+               88 BLACK VALUE 'B'.
+           05 FILLER PIC X.
+           05 PIECE_TYPE PIC X.
+               88 PT-KNIGHT    VALUE 'N'.
+               88 PT-BISHOP    VALUE 'B'.
+               88 PT-ROOK      VALUE 'R'.
+               88 PT-QUEEN     VALUE 'Q'.
+               88 PT-KING      VALUE 'K'.
+           05 FILLER PIC X.
+           05 FROM_SQUARE PIC 99.
+           05 FILLER PIC XX.
+           05 TO_SQUARE PIC 99.
+           05 FILLER PIC X.
+           05 CAPTURE PIC 9.
+           05 PROMOTION PIC X.
+               88 PR-KNIGHT    VALUE 'N'.
+               88 PR-BISHOP    VALUE 'B'.
+               88 PR-ROOK      VALUE 'R'.
+               88 PR-QUEEN     VALUE 'Q'.
+               88 PR-KING      VALUE 'K'.
+           05 CHECK PIC 9.
+           05 CHECKMATE PIC 9.
 
        WORKING-STORAGE SECTION.
        01 WS-EOF PIC X VALUE 'N'.
