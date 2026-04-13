07c1a74d6902da334a07f9bcd3c225dbdc471e7e
WanderingMysz <wanderingmysz@gmail.com>
2026-04-13 12:35:12 -0400

    feat: Add methods for locating appropriate Knight, Rook, or Bishop based on SAN move. Framework for other piecetypes also added.

--- chess-logic/move_validation.c
+++ chess-logic/move_validation.c
@@ -3,6 +3,9 @@
 #include <regex.h>
 #include <stdio.h>
 
+static int WH_KING_IDX = -1;
+static int BL_KING_IDX = -1;
+
 // Returns if a piece matches the given type
 static bool cmp_piece_type(uint8_t piece, PieceType comp) {
     if (comp == NONE) {
@@ -22,6 +25,261 @@ bool piece_exists(Chessboard* board, int idx, uint8_t piece) {
     piece_there |= MOVEMENT_MASK;
 
     return piece == piece_there;
+}
+
+/* -1 Not Found, -2 More than one found */
+/*FIXME Refactor to use check_square method as in _locate_bishop() */
+int _locate_knight(Chessboard* board, bool white, int dest_idx, 
+                    int col, int row) {
+    int offsets[8][2] = {
+        {1,2},
+        {2,1},
+        {2,-1},
+        {1,-2},
+        {-1,-2},
+        {-2,-1},
+        {-2,1},
+        {-1,2}
+    };
+
+    int dest_col = col_from_idx(dest_idx);
+    int dest_row = row_from_idx(dest_idx);
+    int ret_idx = -1;
+
+    for (int i = 0; i < 8; i++) {
+        int src_col = offsets[i][0] + dest_col;
+        int src_row = offsets[i][1] + dest_row;
+
+        // Ignore potential sources outside boundaries
+        if (!(1 <= src_col <= 8)) continue;
+        if (!(1 <= src_row <= 8)) continue;
+
+        // Ignore potential sources that don't line up with given information
+        if (col != 0 && src_col != col) continue;
+        if (row != 0 && src_row != row) continue;
+
+        int src_idx = idx_from_int(src_col, src_row);
+        uint8_t piece_at_idx = board->squares[src_idx];
+
+        // First piece found with right type / color is used
+        if (cmp_piece_type(piece_at_idx, KNIGHT) 
+            && isWhite(piece_at_idx) == white) {
+            if (ret_idx == -1) ret_idx = src_idx;
+            else return -2;
+        }
+    }
+
+    return ret_idx;
+}
+
+/*FIXME Does not break because of intervening pieces. It only checks for rooks
+in the proper col, row. Ideally, should radiate out from dest_idx until piece 
+in the way */
+/*FIXME Refactor to use check_square method as in _locate_bishop() */
+int _locate_rook(Chessboard* board, bool white, int dest_idx, 
+                    int col, int row) {
+
+    int dest_col = col_from_idx(dest_idx);
+    int dest_row = row_from_idx(dest_idx);
+    int ret_idx = -1;
+
+    // Check left and right
+    for (int src_col = 1; src_col <= 8; src_col++) {
+        // Skip destination square
+        if (src_col == dest_col) continue; 
+
+        //TODO Improve to not have check within loop
+        if (col != 0 && src_col != col) continue;
+
+        int src_idx = idx_from_int(src_col, row);
+        uint8_t piece_at_idx = board->squares[src_idx];
+
+        // First piece found with right type / color is used
+        if (cmp_piece_type(piece_at_idx, ROOK) 
+            && isWhite(piece_at_idx) == white) {
+            if (ret_idx == -1) ret_idx = src_idx;
+            else return -2;
+        }
+    }
+
+        // Check left and right
+    for (int src_row = 1; src_row <= 8; src_row++) {
+        // Skip destination square
+        if (src_row == dest_row) continue; 
+        
+        //TODO Improve to not have check within loop
+        if (row != 0 && src_row != row) continue;
+
+        int src_idx = idx_from_int(src_row, col);
+        uint8_t piece_at_idx = board->squares[src_idx];
+
+        // First piece found with right type / color is used
+        if (cmp_piece_type(piece_at_idx, ROOK) 
+            && isWhite(piece_at_idx) == white) {
+            if (ret_idx == -1) ret_idx = src_idx;
+            else return -2;
+        }
+    }
+
+    return ret_idx;
+}
+
+int _locate_bishop(Chessboard* board, bool white, int dest_idx, 
+                    int col, int row) {
+
+    int dest_col = col_from_idx(dest_idx);
+    int dest_row = row_from_idx(dest_idx);
+    int ret_idx = -1;
+
+    bool break_loop = false;
+    
+    // up_right
+    for (int src_col = dest_col; src_col <= 8; src_col++) {
+        if (break_loop) {
+            break_loop = false;
+            break;
+        }
+        for (int src_row = dest_row; src_row <= 8; src_row++) {
+            if (col != 0 && src_col != col) continue;
+            if (row != 0 && src_row != row) continue;
+
+            int src_idx = idx_from_int(src_row, src_col);
+
+            if (check_square(board, BISHOP, white, src_idx))  {
+                if (ret_idx == -1) ret_idx = src_idx;
+                else return -2;
+            }
+            if (!check_square(board, NONE, white, src_idx)) {
+                break_loop = true;
+                break;
+            }
+        }
+    }
+
+    // down_right
+    for (int src_col = dest_col; src_col <= 8; src_col++) {
+        if (break_loop) {
+            break_loop = false;
+            break;
+        }
+
+        for (int src_row = dest_row; 1 <= src_row; src_row--) {
+            if (col != 0 && src_col != col) continue;
+            if (row != 0 && src_row != row) continue;
+
+            int src_idx = idx_from_int(src_row, src_col);
+
+            if (check_square(board, BISHOP, white, src_idx))  {
+                if (ret_idx == -1) ret_idx = src_idx;
+                else return -2;
+            }
+            if (!check_square(board, NONE, white, src_idx)) {
+                break_loop = true;
+                break;
+            }
+        }
+    }
+
+    // down_left
+    for (int src_col = dest_col; 1 <= src_col; src_col--) {
+        if (break_loop) {
+            break_loop = false;
+            break;
+        }
+        for (int src_row = dest_row; 1 <= src_row; src_row--) {
+            if (col != 0 && src_col != col) continue;
+            if (row != 0 && src_row != row) continue;
+
+            int src_idx = idx_from_int(src_row, src_col);
+
+            if (check_square(board, BISHOP, white, src_idx))  {
+                if (ret_idx == -1) ret_idx = src_idx;
+                else return -2;
+            }
+            if (!check_square(board, NONE, white, src_idx)) {
+                break_loop = true;
+                break;
+            }
+        }
+    }
+
+    // up_left
+    for (int src_col = dest_col; src_col <= 8; src_col++) {
+        if (break_loop) {
+            break_loop = false;
+            break;
+        }
+        for (int src_row = dest_row; src_row <= 8; src_row++) {
+            if (col != 0 && src_col != col) continue;
+            if (row != 0 && src_row != row) continue;
+
+            int src_idx = idx_from_int(src_row, src_col);
+
+            if (check_square(board, BISHOP, white, src_idx))  {
+                if (ret_idx == -1) ret_idx = src_idx;
+                else return -2;
+            }
+            if (!check_square(board, NONE, white, src_idx)) {
+                break_loop = true;
+                break;
+            }
+        }
+    }
+
+    return ret_idx;
+}
+
+static bool check_square(Chessboard *board, uint8_t piece_type, bool white, 
+                        int src_idx) {
+    uint8_t piece_at_idx = board->squares[src_idx];
+
+    // None doesn't compare about color
+    if (piece_type == NONE) {
+        return isNone(piece_at_idx);
+    }
+
+    // First piece found with right type / color is used
+    if (cmp_piece_type(piece_at_idx, piece_type) 
+        && isWhite(piece_at_idx) == white) {
+        return true;
+    }
+    return false;
+}
+
+int _locate_queen(Chessboard* board, bool white, int dest_idx, 
+                    int col, int row);
+
+
+int _locate_pawn(Chessboard* board, bool white, int dest_idx, 
+                    int col, int row);
+
+int locate_piece(Chessboard* board, uint8_t piece, int dest_idx, int col, int row) {
+    bool white = isWhite(piece);
+    uint8_t piece_type = piecetype(piece);
+
+    int ret_idx = -1;
+    switch (piece_type) {
+        case KNIGHT:
+            ret_idx = _locate_knight(board, white, dest_idx, col, row);
+            break;
+        case BISHOP:
+            ret_idx = _locate_bishop(board, white, dest_idx, col, row);
+            break;
+        case ROOK:
+            ret_idx = _locate_rook(board, white, dest_idx, col, row);
+            break;
+        case QUEEN:
+            ret_idx = _locate_queen(board, white, dest_idx, col, row);
+            break;
+        case KING:
+            ret_idx = (white) ? WH_KING_IDX : BL_KING_IDX;
+            break;
+        default:
+            ret_idx = _locate_pawn(board, white, dest_idx, col, row);
+            break;
+    }
+
+    return ret_idx;
 }
 
 bool can_castle(Chessboard *board, bool white, bool kingside) {
--- chess-logic/move_validation.h
+++ chess-logic/move_validation.h
@@ -17,4 +17,11 @@ bool is_checkmate(Chessboard *board, bool white);
 /* Returns if the move is in standard algebraic notation (SAN) */
 bool is_SAN(char* move);
 
+/* Returns if the piece exists at the specified square */
+bool piece_exists(Chessboard* board, int idx, uint8_t piece);
+
+/* Returns nearest piece, with col and row as optional restrictors */
+int locate_piece(Chessboard* board, uint8_t piece, int dest_idx, 
+                int col, int row);
+
 #endif /* MOVE_VALIDATION_H */
