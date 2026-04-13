2c4f1da1f8b0e2ae744d7637f0974500abf8bdc8
WanderingMysz <wanderingmysz@gmail.com>
2026-04-12 22:30:25 -0400

    feat: Implement format for chess move record keeping

---
+++ chess-logic/record.h
@@ -0,0 +1,21 @@
+#ifndef RECORD_H
+#define RECORD_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+typedef struct {
+    size_t turn_number; // Turn Number // TODO: Decide if needed
+    bool color; // Current Player
+    uint8_t piece_type; // Piece Moving
+    int src_idx; // Source Square
+    int dest_idx; // Destination Square
+    bool capture; // Capture?
+    uint8_t promotion; // Pawn Promotion?
+    bool check; // Check?
+    bool checkmate; // Checkmate?
+} Move_Record;
+
+void clear_record(Move_Record* move_record);
+
+#endif /* RECORD_H */
