16fddce9e0d9b93ed1b05d863a5d0d3a8bf0f759
WanderingMysz <wanderingmysz@gmail.com>
2026-04-16 14:31:58 -0400

    feat: Add means of parsing SAN into executable move information

--- chess-logic/manipulate_board.h
+++ chess-logic/manipulate_board.h
@@ -3,6 +3,7 @@
 
 #include "types.h"
 #include "piece_info.h"
+#include "record.h"
 
 /* Creates Chessboard */
 Chessboard initialize_chessboard();
@@ -15,5 +16,9 @@ void set_square(Chessboard *board, int idx, uint8_t piece);
 
 /* Removes pieces from every square of the board */
 void clear_board(Chessboard *board);
+
+int get_move_info(Chessboard* board, char* move_str, Move_Record* move_record);
+
+void make_move(Chessboard *board, Move_Record* move);
 
 #endif /* MANIPULATE_BOARD_H */
