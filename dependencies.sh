afeed34fa44404318500129d5adea60527deddae
WanderingMysz <wanderingmysz@gmail.com>
2026-05-18 21:35:28 -0400

    build: Include OS-specific cpanm installation to dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+OS="$(uname -s)"
+
+# OS dependent installations
+case "$OS" in
+  Linux)
+    sudo apt install -y cpanminus
+    ;;
+  Darwin)
+    brew install cpanminus
+    ;;
+  *)
+    curl -L https://cpanmin.us | perl - App::cpanminus
+    ;;
+esac
+
 cpanm --installdeps .
 pip install -r requirements.txt
